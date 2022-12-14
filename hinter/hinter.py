ba09e7d881beab6fc58359450d39bf0b3569a63b
Shelby Shum <sshum00@gmail.com>
2022-12-13 20:17:38 -0800

    added more ghetto code for type hinting, mostly functional now, cleanup in the future

--- hinter/hinter.py
+++ hinter/hinter.py
@@ -1,28 +1,37 @@
 import ast
 import re
-from typing import List, Dict
+from typing import List, Dict, Tuple
 
 
-def parse_function_parameters(function_use: str):
+def parse_function_parameters(function_use: str) -> List:
     pattern = re.compile(r'(?<=\().*(?=\))')
     extracted_value = re.findall(pattern, function_use)[0]
     if extracted_value == '':
-        return ()
+        return []
+    elif extracted_value.startswith("(") and extracted_value.endswith(")"):
+        extracted_literal = ast.literal_eval(extracted_value)
+        return [extracted_literal]
+    else:
+        extracted_value = f"[{extracted_value}]"
     extracted_literal = ast.literal_eval(extracted_value)
-    if not isinstance(extracted_literal, tuple):
-        extracted_literal = tuple([extracted_literal])
     return extracted_literal
 
 
-def get_candidate_based_on_type(function_parameter: str):
+def get_candidate_based_on_type(function_parameter: str) -> str:
     value_type = type(function_parameter)
     additional_params = []
-    if value_type == list:
-        value_type = List
+    if value_type in [list, tuple]:
+        value_type = List if value_type == list else Tuple
         additional_params = list({get_candidate_based_on_type(
             value) for value in function_parameter})
+        additional_params.sort()
     elif value_type == dict:
         value_type = Dict
+        additional_params = list({get_candidate_based_on_type(
+            value) for value in function_parameter})
+        additional_params.extend({get_candidate_based_on_type(
+            value) for value in function_parameter.values()})
+
     if str(value_type).startswith("typing."):
         return (str(value_type).split('typing.')[-1]
                 + str(additional_params).replace("'", ""))
@@ -30,5 +39,26 @@ def get_candidate_based_on_type(function_parameter: str):
     return return_value
 
 
-def generate_recommendation_based_on_usage(function_calls: List[str]):
-    pass
+def replace_old_function_calls_with_new(
+    old_function_call: str, new_param_string: str
+) -> str:
+    pattern = re.compile(r'(?<=\().*(?=\))')
+    return re.sub(pattern, new_param_string, old_function_call)
+
+
+def generate_recommendation_based_on_usage(function_call: str) -> str:
+    parameters = parse_function_parameters(function_call)
+    if not parameters:
+        return function_call
+    print(f'{type(parameters)}')
+    new_param_string = ", ".join(
+        [f'"{parameter}": {get_candidate_based_on_type(parameter)}' if
+         get_candidate_based_on_type(parameter) == "str" else
+            f'{parameter}: {get_candidate_based_on_type(parameter)}'
+         for parameter in parameters])
+    return replace_old_function_calls_with_new(function_call, new_param_string)
+
+
+def generate_recommendation_based_on_usages(function_calls: List[str]) -> List[str]:
+    for function_call in function_calls:
+        pass
--- tests/test_hinter.py
+++ tests/test_hinter.py
@@ -1,18 +1,22 @@
 import pytest
-from typing import List, Dict
 
-from hinter.hinter import parse_function_parameters, get_candidate_based_on_type
+from hinter.hinter import (
+    generate_recommendation_based_on_usage,
+    get_candidate_based_on_type,
+    parse_function_parameters,
+    replace_old_function_calls_with_new
+)
 
 
 @pytest.mark.parametrize(
     "function_call,expected",
     [
-        ("""foo()""", ()),
-        ("""foo(1, 2)""", (1, 2,)),
-        ("""foo("bar", 1)""", ("bar", 1)),
-        ("""foo("bar")""", ("bar", )),
-        ("""foo((1, 2), 'bar')""", ((1, 2), "bar")),
-        ("""foo((1, 2))""", ((1, 2)))
+        ("""foo()""", []),
+        ("""foo(1, 2)""", [1, 2, ]),
+        ("""foo("bar", 1)""", ["bar", 1]),
+        ("""foo("bar")""", ["bar", ]),
+        ("""foo((1, 2), 'bar')""", [(1, 2), "bar"]),
+        ("""foo((1, 2))""", [(1, 2)])
     ]
 )
 def test_parse_function_use(function_call, expected):
@@ -34,3 +38,34 @@ def test_parse_function_use(function_call, expected):
 )
 def test_get_candidate_based_on_type(parameter, expected):
     assert get_candidate_based_on_type(parameter) == expected
+
+
+@pytest.mark.parametrize(
+    "function_call,new_param_string,expected",
+    [
+        ("""foo()""", "", """foo()"""),
+        ("""foo(1, 2)""", "1: int, 2: int", """foo(1: int, 2: int)"""),
+    ]
+)
+def test_replace_old_function_calls_with_new(
+    function_call, new_param_string, expected
+):
+    assert replace_old_function_calls_with_new(
+        function_call, new_param_string) == expected
+
+
+@pytest.mark.parametrize(
+    "function_call,expected",
+    [
+        ("""foo()""", """foo()"""),
+        ("""foo(1, 2)""", """foo(1: int, 2: int)"""),
+        ("""foo("bar", 1)""", """foo("bar": str, 1: int)"""),
+        ("""foo("bar")""", """foo("bar": str)"""),
+        ("""foo((1, 2), 'bar')""", """foo((1, 2): Tuple[int], "bar": str)"""),
+        ("""foo((1, 2))""", """foo((1, 2): Tuple[int])"""),
+        ("""foo([1, 2])""", """foo([1, 2]: List[int])"""),
+        ("""foo({"foo": "bar"})""", """foo({'foo': 'bar'}: Dict[str, str])"""),
+    ]
+)
+def test_generate_recommendation_based_on_usage(function_call, expected):
+    assert generate_recommendation_based_on_usage(function_call) == expected
